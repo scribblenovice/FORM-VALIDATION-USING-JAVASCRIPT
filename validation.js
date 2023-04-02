a43f30a9f93ec5ce6da7899e439665a71d3464d2
scribblenovice <pasomba41@gmail.com>
2023-04-02 15:10:50 +0100

    Upload of the necessary files

---
+++ validation.js
@@ -0,0 +1,72 @@
+const form = document.getElementById("form");
+const username = document.getElementById('username');
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById('password2');
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    validateInputs();
+});
+
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errrorDisplay = inputControl.querySelector('.error');
+    errrorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}
+const setSuccess = (element) => {
+    const inputControl = element.parentElement;
+    const errrorDisplay = inputControl.querySelector('.error');
+    errrorDisplay.innerText = '';
+    inputControl.classList.remove('error');
+    inputControl.classList.add('success');
+}
+const isValidEmail = (email) => {
+    const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if (usernameValue === '') {
+        setError(username, 'Username cannot be empty');
+    } else {
+        setSuccess(username)
+    }
+
+    if (emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address')
+    } else {
+        setSuccess(email);
+    }
+
+    if (passwordValue === '') {
+        setError(password, 'Password is required');
+    }
+    else if (passwordValue.length < 8){
+        setError(password, 'Password must be at least 8 characters');
+    } else {
+        setSuccess(password);
+    }
+
+    if (password2Value === '') {
+        setError(password2, 'Please confirm your password');
+    } else if (password2Value != passwordValue) {
+        setError(password2, 'Password doesn\'t match');
+    } else {
+        setSuccess(password2);
+    }
+}
+
+
